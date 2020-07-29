58e242a131a2cd264bbbdbbf5fb523fc90ec2c5a
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-07-29 00:59:35 -0300

    routes

--- Primeiro Projeto/src/server.ts
+++ Primeiro Projeto/src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-const app = express();
-import routes from './routes'
+import routes from './routes';
 
-app.get('/', (request, response)=>{
-  return response.json({message: 'Hello'});
-})
+const app = express();
+app.use(routes);
+
 
 app.listen(3333, ()=>{console.log('Servidor OK')})
