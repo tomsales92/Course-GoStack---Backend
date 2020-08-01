c03391918abe0698092c990db9a6f4d7b2c50510
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-08-01 10:14:29 -0300

    Cadastrando agendamento

--- Primeiro Projeto/src/routes/appointments.routes.ts
+++ Primeiro Projeto/src/routes/appointments.routes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
+//Nossa persistencia de dados (Provisória)
+const appointments =[];
+
   appointmentsRouter.post('/', (request, response) => {
-  return response.json({message: 'Hello Word'});
+    const {provider, date } = request.body;
+
+    const appointment = {
+      id: uuid(),
+      provider,
+      date,
+    }
+
+    appointments.push(appointment);
+  return response.json(appointment);
   });
 
 export default appointmentsRouter;
