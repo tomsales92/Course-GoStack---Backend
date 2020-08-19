573a2df92bf76660de995e951204a6d7c825c33a
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-08-18 23:52:41 -0300

    Relacionamentos Models

--- Primeiro ProjetoORM/src/database/migrations/1597717502333-CreateUsers.ts
+++ Primeiro ProjetoORM/src/database/migrations/1597717502333-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1597717502333 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
