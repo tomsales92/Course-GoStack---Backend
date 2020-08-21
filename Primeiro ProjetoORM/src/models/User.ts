3134c6471c021da8247ba87520013c64a0a63463
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-08-20 23:49:45 -0300

    Finalizando criação de registros

--- Primeiro ProjetoORM/src/models/User.ts
+++ Primeiro ProjetoORM/src/models/User.ts
@@ -20,9 +20,6 @@ class User {
   @Column()
   password: string;
 
-  @Column('time with time zone')
-  date: Date;
-
   @CreateDateColumn()
   created_at: Date;
 
--- Primeiro ProjetoORM/src/routes/users.routes.ts
+++ Primeiro ProjetoORM/src/routes/users.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import CreateuserService from '../service/CreateUserService';
 
 const UsersRouter = Router();
 
 UsersRouter.post('/', async (request, response) => {
   try {
-    response.send();
+    const { name, email, password } = request.body;
+
+    const createuser = new CreateuserService();
+
+    const user = await createuser.execute({
+      name,
+      email,
+      password,
+    });
+    return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
