5d2c4a533959a9fc998f85e04e044670eb8fca2c
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-08-21 00:06:08 -0300

    Senha hash

--- Primeiro ProjetoORM/src/routes/users.routes.ts
+++ Primeiro ProjetoORM/src/routes/users.routes.ts
@@ -14,6 +14,8 @@ UsersRouter.post('/', async (request, response) => {
       email,
       password,
     });
+
+    delete user.password;
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
