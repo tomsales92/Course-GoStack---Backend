6fdec870d0b583af4df9a8d9e2e5b995d1c1d256
Everton Sales <everton.goncalves92@gmail.com>
2020-10-17 19:26:06 -0300

    Isolando components

--- 06-gobaber-web/src/pages/SignIn/index.tsx
+++ 06-gobaber-web/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <>
@@ -12,10 +14,15 @@ const SignIn: React.FC = () => (
 
         <form>
           <h1>Faça seu logon</h1>
-          <input placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="senha"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
         </form>
         <a href="login">
