32926d848b75e67df99c26c69433d4acbebf88b0
Everton Sales <everton.goncalves92@gmail.com>
2020-10-20 18:31:58 -0300

    Utilizando Unform

--- 06-gobaber-web/src/pages/SignUp/index.tsx
+++ 06-gobaber-web/src/pages/SignUp/index.tsx
@@ -1,40 +1,51 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <>
-    <Container>
-      <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <>
+      <Container>
+        <Background />
 
-      <Content>
-        <img src={logoImg} alt="GoBaber" />
+        <Content>
+          <img src={logoImg} alt="GoBaber" />
 
-        <form>
-          <h1>Faça seu Cadastro</h1>
-          <Input name="nome" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-          <Input
-            name="senha"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+          <Form onSubmit={handleSubmit}>
+            <h1>Faça seu Cadastro</h1>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input
+              name="email"
+              icon={FiMail}
+              type="text"
+              placeholder="E-mail"
+            />
+            <Input
+              name="senha"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
 
-          <Button type="submit">Cadastrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
-        </form>
-        <a href="login">
-          <FiArrowLeft />
-          Voltar para logon
-        </a>
-      </Content>
-    </Container>
-  </>
-);
+            <Button type="submit">Cadastrar</Button>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
+          <a href="login">
+            <FiArrowLeft />
+            Voltar para logon
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
 
 export default SignUp;
