016a79cb3fa3df2b637e188f3f9a2877dc2ce858
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-10-26 23:18:26 -0300

    Criando tooltip de erros e Valudação de login

--- 06-gobaber-web/src/pages/SignIn/index.tsx
+++ 06-gobaber-web/src/pages/SignIn/index.tsx
@@ -1,38 +1,67 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
+import getValidatorErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <>
-    <Container>
-      <Content>
-        <img src={logoImg} alt="GoBaber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <form>
-          <h1>Faça seu logon</h1>
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="senha"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatótio')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha inválida'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
 
-          <Button type="submit">Entrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
-        </form>
-        <a href="login">
-          <FiLogIn />
-          Criar Conta
-        </a>
-      </Content>
-      <Background />
-    </Container>
-  </>
-);
+      const errors = getValidatorErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+  return (
+    <>
+      <Container>
+        <Content>
+          <img src={logoImg} alt="GoBaber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit">Entrar</Button>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
+          <a href="login">
+            <FiLogIn />
+            Criar Conta
+          </a>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
